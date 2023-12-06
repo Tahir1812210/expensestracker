0c40bb5d187acc7cd5dddacc768bef2af39c0eea
Tahirid1812210 <cs1812210@szabist.pk>
2023-12-06 20:39:41 +0300

    expense filtering is done

---
+++ src/expense-tracker/ExpenseFilter.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface Props {
+  onSelectCategory: (category: string) => void;
+}
+
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
+  return (
+    <select
+      className="form-select"
+      onChange={(event) => onSelectCategory(event.target.value)}
+    >
+      <option value="">All categories</option>
+      <option value="Grocerries">Grocerries</option>
+      <option value="Utilities">Utilities</option>
+      <option value="Entertainment">Entertainment</option>
+    </select>
+  );
+};
+
+export default ExpenseFilter;
